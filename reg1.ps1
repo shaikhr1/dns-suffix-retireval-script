0fd77d86667f73fa3fca307eef613196e8de195a
Rizwan Shaikh <122839389+shaikhr1@users.noreply.github.com>
2023-01-16 22:09:11 -0500

    Add files via upload

---
+++ reg1.ps1
@@ -0,0 +1,24 @@
+$registryPath = "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters"
+$registryKey = Get-Item -Path $registryPath
+
+$registryValue = Get-ItemProperty -Path $registryKey.PSPath
+
+Write-Output $registryValue
+
+# Check if user wants to delete Domain key value
+$deleteDomain = Read-Host -Prompt "Do you want to delete the Domain key value? (Y/N)"
+if ($deleteDomain -eq "Y") {
+    Remove-ItemProperty -Path $registryKey.PSPath -Name "Domain"
+}
+
+# Check if user wants to delete NV Domain key value
+$deleteNVDomain = Read-Host -Prompt "Do you want to delete the NV Domain key value? (Y/N)"
+if ($deleteNVDomain -eq "Y") {
+    Remove-ItemProperty -Path $registryKey.PSPath -Name "NV Domain"
+}
+
+# Check if user wants to delete SearchList key value
+$deleteSearchList = Read-Host -Prompt "Do you want to delete the SearchList key value? (Y/N)"
+if ($deleteSearchList -eq "Y") {
+    Remove-ItemProperty -Path $registryKey.PSPath -Name "SearchList"
+}
